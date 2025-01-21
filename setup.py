154bd859eae8701530596b87db070c1f7431f824
Daniel W. Anner <daniel.anner@danstechsupport.com>
2025-01-21 17:47:39 +0000

    Updating deployment and setup scripts

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
+VERSION = "2.2.0"  # PEP-440
+
+NAME = "BulletPrompt"
+
+INSTALL_REQUIRES = []
+
 
 setup(
-      name='bullet',
-      version='2.2.0',
-      description='Beautiful Python prompts made simple.',
-      long_description="Extensive support for Python list prompts \
-            formatting and colors",
-      url='https://github.com/Mckinsey666/bullets',
-      keywords = "cli list prompt customize colors",
-      author='Mckinsey666',
-      license='MIT',
-      packages=find_packages(),
-      python_requires=">=3.6"
+    name=NAME,
+    version=VERSION,
+    description="Beautiful Python prompts made simple. Maintained version of bchao1/bullet.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/danner26/BulletPrompt",
+    project_urls={
+        "Source Code": "https://github.com/danner26/BulletPrompt",
+    },
+    author="danner26",
+    author_email="daniel.anner@danstechsupport.com",
+    keywords = "cli list prompt customize colors",
+    license="MIT",
+    python_requires=">=3.6",
+    # Requirements
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
 )
